d4080d1a2aca38cf5e69ee4e7f03615a386bba36
Oleksandr Zenchenko <Oleksandr_Zenchenko@epam.com>
2022-12-17 08:32:39 +0100

    refactoring

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,18 +1,43 @@
-function createChart (data) {
-  const myChart = new Chart("acquisitions", {
-    type: 'line',
-    data: {
-      labels: data.map(row => row.date),
-      datasets: [
-        {
-          label: 'Open prices',
-          data: data.map(row => row.open)
-        },
-        {
-          label: 'Close prices',
-          data: data.map(row => row.close)
-        }
-      ]
+$(document).ready(function() {
+  class QuoteChart {
+    constructor() {
+      this.init(this.readData());
     }
-  });
-}
+
+    init(data) {
+      const myChart = new Chart("acquisitions", {
+        type: 'line',
+        data: {
+          labels: data.map(row => row.date),
+          datasets: [
+            {
+              label: 'Open prices',
+              data: data.map(row => row.open)
+            },
+            {
+              label: 'Close prices',
+              data: data.map(row => row.close)
+            }
+          ]
+        }
+      });
+    }
+
+    readData() {
+      const $rows = document.querySelectorAll('tr.price-data');
+      const data = [];
+      for (const $row of $rows) {
+        data.unshift({
+            date: $($row).data('date'),
+            open: $($row).data('open'),
+            close: $($row).data('close'),
+          });
+        console.log($row);
+      }
+
+      return data;
+    }
+  }
+
+  chart = new QuoteChart();
+})
